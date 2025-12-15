8830077735c6c7b4e9c3c4bac844140579533563
Phil Wee <cyber_peewee@yahoo.com>
2025-12-15 09:05:13 -0800

    add dark mode logo and icon images

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -5,8 +5,30 @@ interface LogoProps {
 
 export function Logo({ className, animate = false }: LogoProps) {
   return animate ? (
-    <img className={className} src="/icon.png" alt="icon" />
+    <>
+      <img
+        className={`${className} block dark:hidden`}
+        src="/icon.png"
+        alt="icon"
+      />
+      <img
+        className={`${className} hidden dark:block`}
+        src="/icon-dark.png"
+        alt="icon"
+      />
+    </>
   ) : (
-    <img className={className} src="/logo.png" alt="logo" />
+    <>
+      <img
+        className={`${className} block dark:hidden`}
+        src="/logo.png"
+        alt="logo"
+      />
+      <img
+        className={`${className} hidden dark:block`}
+        src="/logo-dark.png"
+        alt="logo"
+      />
+    </>
   );
 }
